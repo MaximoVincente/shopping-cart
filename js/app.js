81e364f37a4361bcfc45fae9da1195a4907edf0c
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-09-01 17:03:10 -0400

    Finished app.js

--- js/app.js
+++ js/app.js
@@ -5,18 +5,25 @@ const Cart = function(items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
-
+this
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // DONE: Fill in this instance method to create a new CartItem and add it to this.items
+let thing = new CartItem(product,quantity);
+this.items.push(thing);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  // DONE: Fill in this instance method to save the contents of the cart to localStorage
+  let cart = Cart.cartItem;
+  localStorage.setItem('cart'), JSON.stringify(cart);
+  renderCart();
 };
 
 Cart.prototype.removeItem = function(item) {
-  // TODO: Fill in this instance method to remove one item from the cart.
+  // DONE: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  let remove = new CartItem(item);
+  this.thing.pop(remove);
 };
 
 const CartItem = function(product, quantity) {
